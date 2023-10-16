b07495b12bad7f45293c64ed13e520fe26f48be5
bobmoran47 <146392046+bobmoran47@users.noreply.github.com>
2023-10-16 09:26:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 data "aws_ami" "app_ami" {
   most_recent = true
 
@@ -68,7 +67,29 @@ module "acme_alb" {
       target_type      = "instance"
     }
   ]
-}
- 
 
- 
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+module "acme_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+
+  vpc_id  = module.acme_vpc.vpc_id
+  name    = "acme"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
